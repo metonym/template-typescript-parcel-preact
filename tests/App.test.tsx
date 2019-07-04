6a94b1a4fe20cd2f6178795b85ad7bf128ab6430
Eric Liu <ericyl.us@gmail.com>
2019-07-04 08:48:40 -0700

    chore: move tests folder

--- tests/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { h, render } from 'preact';
-import App from '../src/App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App />, div);
-});
