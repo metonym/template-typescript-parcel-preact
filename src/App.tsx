5440cabbb5f551135317015c3efc5dfd62befb1c
Eric Liu <ericyl.us@gmail.com>
2019-10-12 18:58:03 -0700

    refactor(src): convert App to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import { Component, Fragment, h } from 'preact';
+import { Fragment, h } from 'preact';
 
-class App extends Component {
-  public render() {
-    return <Fragment>App</Fragment>;
-  }
+function App() {
+  return <Fragment>App</Fragment>;
 }
 
 export default App;
